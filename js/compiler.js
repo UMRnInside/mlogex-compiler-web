9a745c815b04eaea8880be0e4f573d6629aab800
UMRnInside <30196401+UMRnInside@users.noreply.github.com>
2021-10-07 11:04:06 +0800

    Support decompilation

--- js/compiler.js
+++ js/compiler.js
@@ -23,7 +23,32 @@ except ValueError:
 except:
     js.msgArea.innerText = "Internal error"
 else:
-    js.msgArea.innerText = "Success!"
+    js.msgArea.innerText = "Compilation complete!"
+`)
+}
+
+function decompile() {
+    srcArea = document.getElementById("source-code-area")
+    dstArea = document.getElementById("destination-code-area")
+    msgArea = document.getElementById("message1")
+    pyodide.runPython(`
+import js
+from mlog_extended import BasicDecompiler, CompilationError
+decompiler = BasicDecompiler()
+
+try:
+    js.msgArea.innerText = "Decompiling..."
+    src = js.srcArea.value
+    result = decompiler.decompile(src)
+    js.dstArea.value = result
+except CompilationError as e:
+    js.msgArea.innerText = e.args[0]
+except ValueError:
+    js.msgArea.innerText = "Internal error or wrong kwarg format!"
+except:
+    js.msgArea.innerText = "Internal error"
+else:
+    js.msgArea.innerText = "Decompilation complete!"
 `)
 }
 
